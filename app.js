260b133aff19b99820d2889b2351172eebbfb2b1
matheusmagenta <matheus.magenta@gmail.com>
2021-02-21 16:55:11 +0000

    implemeting initial game logic

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 "use strict";
 
+// selecting elements
+const board = document.querySelector(".board");
+const playerHand1 = document.querySelector(".player1");
+const playerHand2 = document.querySelector(".player2");
+const playerHand3 = document.querySelector(".player3");
+const playerHand4 = document.querySelector(".player4");
+
+const btnPlay = document.querySelector(".play");
+
 // creating tiles
 const dominoTiles = [
   [0, 0],
@@ -41,16 +50,16 @@ const player4 = [];
 // creating table
 const table = [];
 
-// creating function to draw 7 random tiles to each player
+// creating function to shuffle 7 random tiles to each player
 function divideTiles(dominoTiles, player) {
   let i = 0;
   while (i < 7) {
     // draw the tile
-    let tileDrawnIndex = Math.floor(Math.random() * dominoTiles.length);
+    let tileShuffleIndex = Math.floor(Math.random() * dominoTiles.length);
     // take the tile from the pile
-    let tileDrawn = dominoTiles.splice(tileDrawnIndex, 1);
+    let tileShuffle = dominoTiles.splice(tileShuffleIndex, 1);
     // add the pile drawn to player
-    player.push(tileDrawn);
+    player.push(tileShuffle);
     i++;
   }
   //console.log("remaining tiles: ", dominoTiles.length);
@@ -62,22 +71,69 @@ function gameInit() {
   divideTiles(dominoTiles, player2);
   divideTiles(dominoTiles, player3);
   divideTiles(dominoTiles, player4);
-  console.log("the game begins: ", player1, player2, player3, player4);
+  //console.log("the game begins: ", player1, player2, player3, player4);
+  showTiles(player1, playerHand1);
+  showTiles(player2, playerHand2);
+  showTiles(player3, playerHand3);
+  showTiles(player4, playerHand4);
+}
+
+/* class Player {
+  constructor(hand, points) {
+    (this.hand = hand), (this.points = points);
+  }
+}
+ */
+
+function showTiles(player, playerDiv) {
+  player.map((tile) => {
+    const pTile = document.createElement("p");
+    pTile.innerHTML = `<p>[${tile[0][0]}|${tile[0][1]}]</p>`;
+    //console.log(pTile);
+    playerDiv.appendChild(pTile);
+  });
+}
+
+function gameSequence() {}
+
+btnPlay.addEventListener("click", gameInit);
+
+const tableDiv = document.querySelector(".table");
+
+// adding tiles to the table
+// NEED TO BE ATTACHED TO THE RULES OF NEXT MOVE
+/* window.addEventListener("click", function (e) {
+  if (e.target.nodeName === "P") {
+    tableDiv.appendChild(e.target);
+  } else {
+    console.log("not a tile");
+  }
+}); */
+
+window.addEventListener("click", move);
+
+//move
+function move(e) {
+  const tile = e.target;
+  if (e.target.nodeName === "P") {
+    console.log(e.target.parentElement.parentElement.className);
+    let chosenTile = e.target.parentElement.innerText;
+    console.log("chosenTile: ", chosenTile);
+    console.log(e.target.parentNode.parentNode);
+    tableDiv.appendChild(e.target);
+  } else {
+    console.log("not a tile");
+  }
 }
 
 // hard coding game
 // initial move
+
 function firstMove(player, tileIndex) {
   chosenTile = player[tileIndex][0];
   table.push(chosenTile);
   delete player[tileIndex][0];
   console.log("table: ", table);
-}
-
-// game sequence
-function gameSequence() {
-  firstMove(player1, tileIndex);
-  nextMove(player2, tileIndex);
 }
 
 // next move
